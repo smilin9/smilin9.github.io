5f4116563185dec7e6716c5bdb25ea5287e7255a
smilin9 <qliu_6@stu.xidian.edu.cn>
2022-11-02 15:55:59 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function () {
     let mySrc = myImage.getAttribute('src');
-    if (mySrc === 'images/firefox-icon.png') {
-        myImage.setAttribute('src', 'images/firefox2.png');
+    if (mySrc === 'images/test.jpg') {
+        myImage.setAttribute('src', 'images/test2.jpg');
     } else {
-        myImage.setAttribute('src', 'images/firefox-icon.png');
+        myImage.setAttribute('src', 'images/test.jpg');
     }
 }
 let myButton = document.querySelector('button');
@@ -16,14 +16,14 @@ function setUserName() {
         setUserName();
     } else {
         localStorage.setItem('name', myName);
-        myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+        myHeading.textContent = '太帅了，' + myName;
     }
 }
 if (!localStorage.getItem('name')) {
     setUserName();
 } else {
     let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+    myHeading.textContent = '太帅了，' + storedName;
 }
 myButton.onclick = function () {
     setUserName();
